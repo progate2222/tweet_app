b7347d0e7fe88add6c52785d79fb4f9532a7bf21
sayaka tamura <goooodnight@yahoo.co.jp>
2022-07-26 13:13:16 +0900

    投稿機能の実装

---
+++ db/schema.rb
@@ -0,0 +1,13 @@
+
+ActiveRecord::Schema.define(version: 2022_07_26_023014) do
+
+
+  enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
